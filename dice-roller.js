8d12a065663b02ab885b607bc61a94575a497600
ChrisProbyn <cprobyn16@gmail.com>
2018-11-27 20:29:50 +0000

    fixed dice roller

--- dice-roller.js
+++ dice-roller.js
@@ -3,7 +3,7 @@ var args = process.argv;
 function diceRoller(number){
   var string = "Rolled " + number + " dice: ";
   for(var i = 0; i < number; i++){
-    string += Math.floor(Math.random() * 7);
+    string += Math.floor((Math.random() * 6)+1);
     if(i !== number - 1){
       string += ", ";
     }
